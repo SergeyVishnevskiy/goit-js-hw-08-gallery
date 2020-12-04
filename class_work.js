ac3189618b07ad1ad85839fcc06278bd3a22f257
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-12-04 14:51:28 +0200

    class work

--- class_work.js
+++ class_work.js
@@ -2,15 +2,21 @@ import galleryItems from "./gallery-items.js";
 
 class Gallery {
   constructor() {
-    const listGalleryRef = document.querySelector(".js-gallery");
-    const modalRef = document.querySelector(".js-lightbox");
-    const imageModalRef = document.querySelector(".lightbox__image");
-    const closeModalButton = document.querySelector(
+    this.listGalleryRef = document.querySelector(".js-gallery");
+    this.modalRef = document.querySelector(".js-lightbox");
+    this.imageModalRef = document.querySelector(".lightbox__image");
+    this.closeModalButton = document.querySelector(
       'button[data-action="close-lightbox"]'
     );
-    const overlayRef = document.querySelector(".lightbox__overlay");
-    let indexCurrentImage;
+    this.overlayRef = document.querySelector(".lightbox__overlay");
+    this.indexCurrentImage = 0;
   }
 }
 
 const gallery = new Gallery();
+console.log(gallery.listGalleryRef);
+console.log(gallery.modalRef);
+console.log(gallery.imageModalRef);
+console.log(gallery.closeModalButton);
+console.log(gallery.overlayRef);
+console.log(gallery.indexCurrentImage);
