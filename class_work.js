3114addc0209911001c90cc73bc3a06bc1024439
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-12-04 14:40:36 +0200

    class_work

--- class_work.js
+++ class_work.js
@@ -1,1 +1,16 @@
 import galleryItems from "./gallery-items.js";
+
+class Gallery {
+  constructor() {
+    const listGalleryRef = document.querySelector(".js-gallery");
+    const modalRef = document.querySelector(".js-lightbox");
+    const imageModalRef = document.querySelector(".lightbox__image");
+    const closeModalButton = document.querySelector(
+      'button[data-action="close-lightbox"]'
+    );
+    const overlayRef = document.querySelector(".lightbox__overlay");
+    let indexCurrentImage;
+  }
+}
+
+const gallery = new Gallery();
